141d8066fed7053c08018aee56ce2a4de1b16f0c
Ingrid <ingriidsilva2016@gmail.com>
2021-10-15 11:03:22 -0300

    First commit

---
+++ aula-01.php
@@ -0,0 +1,27 @@
+1º Problema: Não conseguir voltar alterações no projeto
+2º Problema: Um programador sobescrever o código do outro
+
+Solução:
+    Versionamento
+
+Repositório - Projeto no GIT
+
+Configurações
+    git config --global user.name
+    git config --global user.email
+    git config --global core.editor
+    git config --list
+
+
+Iniciando um repositório
+
+git init - Inicializar Repositório GIT
+
+Branch - Versões do sistema
+Versão principal: Master
+
+commit - salvar alterações no git
+
+README - Instruções/descrição/comentários do projeto
+
+git add -A - Para adcionar ao container
