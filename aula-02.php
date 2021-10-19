797f6e6a9e5cd88ba92c72247098818972d09424
Ingrid <ingriidsilva2016@gmail.com>
2021-10-19 10:19:19 -0300

    teste

--- aula-02.php
+++ aula-02.php
@@ -8,3 +8,5 @@ Revertendo Modificações
 2-git reset -soft 'commit' - alterações ficam preparadas para commit - volta ao estado antes do commit
 2-git reset -mixed 'commit' -alteraçõess não preparadas para commit - tem que dar add outra vez
 2-git reset -hard 'commit' - perde as alterações
+
+teste
