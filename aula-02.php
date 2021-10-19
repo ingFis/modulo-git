e2e6e91462f131ecc0e3a875bd193533e8ab17f7
Ingrid <ingriidsilva2016@gmail.com>
2021-10-19 09:59:28 -0300

    third commit

--- aula-02.php
+++ aula-02.php
@@ -1,1 +1,10 @@
 ver Branch - git branch
+
+git commit -am 'second commit' - add + commit 
+
+Revertendo Modificações
+
+1- git log
+2-git reset -soft 'commit' - alterações ficam preparadas para commit - volta ao estado antes do commit
+2-git reset -mixed 'commit' -alteraçõess não preparadas para commit - tem que dar add outra vez
+2-git reset -hard 'commit' - perde as alterações
