3cb9c1fa4630917c22907c324617f99bae87543d
Ingrid <ingriidsilva2016@gmail.com>
2021-10-19 10:20:16 -0300

    forth commit

--- aula-02.php
+++ aula-02.php
@@ -2,11 +2,17 @@ ver Branch - git branch
 
 git commit -am 'second commit' - add + commit 
 
-Revertendo Modificações
+1- Revertendo Modificações
 
-1- git log
-2-git reset -soft 'commit' - alterações ficam preparadas para commit - volta ao estado antes do commit
-2-git reset -mixed 'commit' -alteraçõess não preparadas para commit - tem que dar add outra vez
-2-git reset -hard 'commit' - perde as alterações
+1.1- git log
+1.2-git reset -soft 'commit o que vai apagar' - alterações ficam preparadas para commit - volta ao estado antes do commit
+1.2-git reset -mixed 'commit o último que é para ficar' -alteraçõess não preparadas para commit - tem que dar add outra vez
+1.2-git reset -hard 'commit' - perde as alterações
 
-teste
+2 -Trabalhando com diferentes Branches
+
+git branch nome - Cria novo histórico a partir do master
+
+git checkout nome - para trocar de branch
+
+se commitar em outra branch e depois voltar para a master ela não terá reconhecido 
